1b3b49e72d2f3a70da269d8bd4703691d2be39d1
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2025-03-13 15:30:30 +0000

    Add scroll reveal effect for enhanced visibility of elements in main.js

--- js/main.js
+++ js/main.js
@@ -54,6 +54,22 @@
     $('.time').datetimepicker({
         format: 'LT'
     });
+ 
+document.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('scroll', revealOnScroll);
+  function revealOnScroll() {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add('active');
+      }
+    }
+  }
+  revealOnScroll();
+});
 
 
     // Header carousel
